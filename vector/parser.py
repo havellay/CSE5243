23e27ac7afec11c5aa65c50f351d6a1ac93f919c
havellay <hari939@gmail.com>
2014-09-12 13:43:01 -0400

    Get started already

---
+++ vector/parser.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+REUTERS_DIR = '../reuters/'
+
+class parser():
+    def 
+
+
+def main():
+    # find all the files in the reuters directory
+    os.chdir(REUTERS_DIR)
+    for file in glob.glob("file")
+
+
+
+    # call the vector finder class or something for each file
+
+if __name__ == "main":
+    main()
