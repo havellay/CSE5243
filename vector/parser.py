c3587c1de186e7fd608e8ad78244b7cc34fe2c9b
havellay <hari939@gmail.com>
2014-09-13 22:13:47 -0400

    Parser can now create <REUTERS> tags

--- vector/parser.py
+++ vector/parser.py
@@ -10,18 +10,25 @@ FAIL = 1
 DONE = 0
 
 class Article:
-    def __init__(self, x, buff):
-        self.id         = x
-        self.full_text  = buff
+    def __init__(self, x):
+        self.id     = x                 # an 'id' is actually not needed at all
+
+        self.tags   = {}                # a dictionary containing all the tags
+                                        # in an article
+
+    def take_this_tag(self, tag_name, tag_text, tag_tokens):
+        self.tags[tag_name] = Tag(tag_name, tag_text, tag_tokens)
+        # should we make sure whether there is already a tag with this name? It
+        # could become an issue for tags such as <D> of which there might already
+        # be a copy
 
 class Tag:
-    def __init__(self, name):
-        self.name = name
-        # should find some way of returning two packed tag objects?
+    def __init__(self, name, text='', tokens=[]):
+        self.name   = name
+        self.text   = text
+        self.tokens = tokens
 
-    def take_full_text(self, fp):
-        buff    = ''
-
+    def tagify_to_article(self, article, fp):
         while True:
             s = fp.readline()
             if not s:
@@ -30,7 +37,7 @@ class Tag:
 
             if '<'+self.name in s:
                 startfrom   = s.index('<'+self.name)
-                buff        = s[startfrom:]+' '
+                self.text   += s[startfrom:]+' '
                 in_this_tag = True
             elif in_this_tag is True:
                 if '</'+self.name+'>' in s:
@@ -38,23 +45,39 @@ class Tag:
                     in_this_tag = False
                 else:
                     endat   = len(s)
-                buff += s[0:endat]+' '
+                self.text   += s[0:endat]+' '
                 if in_this_tag is False:
-                    article_list.append(Article(len(article_list)+1, buff))
+                    # we are done processing the tag in question
+                    # and so, can break here
+                    break
+
+        # we have all the text at this point; we should
+        # do the token processing at this stage.
+
+        # this new tag should be appended to an Article
+        article.take_this_tag(self.name, self.text, self.tokens)
+        self.text   = ''
+        self.tokens = []
         return fp
 
-
 class Parser:
-    def __init__():
+    tags = {}
+
+    def __init__(self):
         # create instances for all kinds of tags
-        interesting_tags = ['DATE', 'TOPICS', 'PLACES', 'D',
+        interesting_tags = ['REUTERS', 'DATE', 'TOPICS', 'PLACES', 'D',
                             'PEOPLE', 'ORGS', 'EXCHANGES', 'COMPANIES',
                             'UNKNOWN', 'TEXT', 'DATELINE', 'BODY']
-        tags = {}
-        for tagnames in interesting_tags:
-            tags[tagnames] = Tag(tagnames)
 
-    def process_file(f):
+        # 'tags' is a dictionary containing instances of tags that are just
+        # meant to be worker tag objects; they won't hold any data in them
+        # each tag will have methods that process plain text and invoke
+        # an article's take_this_tag() method which clones the tag and then,
+        # the contents of the worker tags are cleared
+        for tagnames in interesting_tags:
+            self.tags[tagnames] = Tag(tagnames)
+
+    def process_file(self, f):
         with open(REUTERS_DIR+f) as fp:
             # process 'REUTERS' from here; that will take care of the rest
             # of the tags because :
@@ -68,13 +91,14 @@ class Parser:
 
                 # seek the file object to the beginning of the 'REUTERS' tag
                 while '<REUTERS' not in s:
-                    s.readline()
+                    s = fp.readline()
                 length = len(s) - s.index('<REUTERS')
                 fp.seek(-1*length, 1)
 
                 # now, fp is where 'REUTERS' tag begins
-                # HARI : may not need this 'fp = ...'
-                fp = tags['REUTERS'].take_full_text(fp)
+                article = Article(len(article_list)+1)
+                article_list.append(article)
+                self.tags['REUTERS'].tagify_to_article(article, fp)
 
         print 'DONE WITH {}'.format(f)
         return DONE
@@ -89,4 +113,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print len(article_list)
+    print 'Processed {} articles'.format(len(article_list))
