6a5463c44c4b8cb9e89c2f02e62639580546232b
havellay <hari939@gmail.com>
2014-09-18 02:05:58 -0400

    Adding README and run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# This file launches the Python script that will launch our feature vector 
+# creation script. The script will take to a 'pdb' terminal from where all
+# data members of the script can be examined
+
+# Parser.py contains main() and launches all other scripts
+# dataset is contained with the source
+
+`which python` Parser.py
